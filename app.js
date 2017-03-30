77a93ffede23ca19ea82de3b98a3c3831f52b971
Laios142 <floflo142@hotmail.fr>
2017-03-30 13:22:14 +0200

    Change 30/03

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 require('./config');
+
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", require('./web'));
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 3000;
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log(`App running on port: ${port}`);
-});
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.get('/', function(req, res, next) {
-    // Handle the get for this route
-});
-
-app.post('/', function(req, res, next) {
-    // Handle the post for this route
 });
