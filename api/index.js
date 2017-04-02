0ae6ee6fbbc811e412c223659dbc951494b29b6d
Laios142 <floflo142@hotmail.fr>
2017-04-02 15:01:44 +0200

    jwt done

--- api/index.js
+++
@@ -1,6 +0,0 @@
-var router = require('express').Router();
-
-router.use('/public', require('./public'));
-router.use('/prive', require('./prive'));
-
-module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var jwt = require("jwt-simple");
+var auth = require("./auth.js")();
+var users = require("./models/users.js");  
+var cfg = require ("./config.js")
 require('./config');
 
 
@@ -9,12 +13,44 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
+app.use(auth.initialize());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", require('./web'));
+app.get("/user", auth.authenticate(), function(req, res) {  
+    res.json(users[req.user.id]);
+});
+
+
+app.post("/token", function(req, res) {  
+    if (req.body.email && req.body.password) {
+        var email = req.body.email;
+        var password = req.body.password;
+        var user = users.find(function(u) {
+            return u.email === email && u.password === password;
+        });
+        if (user) {
+            var payload = {
+                id: user.id
+            };
+            var token = jwt.encode(payload, cfg.jwtSecret);
+            res.json({
+                token: token
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+
+
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
     console.log(`App running on port: ${port}`);
 });
+
+module.exports = app;
